f3fe29a0f38113c0a1e48db685baf1ee455d056e
Romain Acciari <romain.acciari@gmail.com>
2017-02-21 16:28:57 +0100

    oioproxy: Add preferMaster/preferSlave/preferMasterForWrites/forceMaster options

--- manifests/oioproxy.pp
+++ manifests/oioproxy.pp
@@ -1,16 +1,20 @@
 # Configure and install an OpenIO oioproxy service
 define openiosds::oioproxy (
-  $action         = 'create',
-  $type           = 'oioproxy',
-  $num            = '0',
+  $action                = 'create',
+  $type                  = 'oioproxy',
+  $num                   = '0',
 
-  $ns             = undef,
-  $ipaddress      = $::ipaddress,
-  $port           = $::openiosds::params::oioproxy_port,
-  $debug          = false,
-  $prefermaster   = undef,
+  $ns                    = undef,
+  $ipaddress             = $::ipaddress,
+  $port                  = $::openiosds::params::oioproxy_port,
+  $debug                 = false,
+  $cmdOptions            = undef,
+  $preferMaster          = undef,
+  $preferSlave           = undef,
+  $preferMasterForWrites = undef,
+  $forceMaster           = undef,
 
-  $no_exec        = false,
+  $no_exec               = false,
 ) {
 
   if ! defined(Class['openiosds']) {
@@ -26,10 +30,31 @@ define openiosds::oioproxy (
   if ! has_interface_with('ipaddress',$ipaddress) { fail("${ipaddress} is invalid.") }
   validate_integer($port)
   validate_bool($debug)
-  if $debug { $verbose = '-v ' }
-  if $prefermaster {
-    validate_string($prefermaster)
-    $_prefermaster = "-O PreferMaster=${prefermaster}"
+  if $cmdOptions {
+    validate_string($cmdOptions)
+    $_cmdoptions = $cmdOptions
+  }
+  else {
+    if $debug { $verbose = '-v ' }
+    if $preferMaster {
+      validate_string($preferMaster)
+      $_preferMaster = "-O PreferMaster=${preferMaster}"
+    }
+    if $preferSlave {
+      validate_string($preferSlave)
+      $_preferSlave = "-O PreferSlave=${preferSlave}"
+    }
+    if $preferMasterForWrites {
+      validate_string($preferMasterForWrites)
+      $_preferMasterForWrites = "-O PreferMasterForWrites=${preferMasterForWrites}"
+    }
+    if $forceMaster {
+      validate_string($forceMaster)
+      $_forceMaster = "-O ForceMaster=${forceMaster}"
+    }
+    $_cmdoptions = join(([$verbose, $_preferMaster, $_preferSlave,
+      $_preferMasterForWrites, $_forceMaster].reduce([]) |$memo, $x|
+      { if $x == undef { $memo } else {$memo << $x} }),' ')
   }
 
 
@@ -50,7 +75,7 @@ define openiosds::oioproxy (
   # Init
   gridinit::program { "${ns}-${type}-${num}":
     action  => $action,
-    command => "${openiosds::bindir}/oio-proxy ${verbose} ${_prefermaster} -p ${openiosds::runstatedir}/${ns}-${type}-${num}.pid -s OIO,${ns},${type},${num} ${ipaddress}:${port} ${ns}",
+    command => "${openiosds::bindir}/oio-proxy -p ${openiosds::runstatedir}/${ns}-${type}-${num}.pid -s OIO,${ns},${type},${num} ${ipaddress}:${port} ${_cmdoptions} ${ns}",
     group   => "${ns},${type},${type}-${num}",
     uid     => $openiosds::user,
     gid     => $openiosds::group,

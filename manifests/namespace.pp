96bdfd123cfde94fa6e5b1bd0f6fdc1d21e4f252
Lapierre <sebastien@openio.io>
2018-03-15 15:34:55 +0100

    Add more ns parameters and make ratelimit in oioswift pipeline optional

--- manifests/namespace.pp
+++ manifests/namespace.pp
@@ -18,6 +18,37 @@ define openiosds::namespace (
   $events_common_pending_max = undef,
   $ns_storage_policy         = 'THREECOPIES',
   $ns_chunk_size             = undef,
+  $sqliterepo_election_delay_ping_final = undef,
+  $sqliterepo_election_delay_expire_slave = undef,
+  $sqliterepo_election_delay_expire_master = undef,
+  $sqliterepo_election_delay_expire_none = undef,
+  $sqliterepo_outgoing_timeout_cnx_use = undef,
+  $sqliterepo_outgoing_timeout_req_use = undef,
+  $sqliterepo_outgoing_timeout_cnx_getvers = undef,
+  $sqliterepo_outgoing_timeout_req_getvers = undef,
+  $sqliterepo_outgoing_timeout_cnx_replicate = undef,
+  $sqliterepo_outgoing_timeout_req_replicate = undef,
+  $sqliterepo_outgoing_timeout_cnx_resync = undef,
+  $sqliterepo_outgoing_timeout_req_resync = undef,
+  $sqliterepo_election_wait_delay = undef,
+  $sqliterepo_election_wait_quantum = undef,
+  $sqliterepo_election_nowait_after = undef,
+  $proxy_outgoing_timeout_stat = undef,
+  $proxy_outgoing_timeout_conscience = undef,
+  $proxy_outgoing_timeout_common = undef,
+  $meta0_outgoing_timeout_common_req = undef,
+  $meta1_outgoing_timeout_common_req = undef,
+  $gridd_timeout_connect_common = undef,
+  $resolver_cache_srv_max_default = undef,
+  $resolver_cache_csm0_max_default = undef,
+  $sqliterepo_cache_ttl_cool = undef,
+  $sqliterepo_cache_ttl_hot = undef,
+  $client_errors_cache_enabled = undef,
+  $client_errors_cache_period = undef,
+  $client_errors_cache_max = undef,
+  $client_down_cache_avoid = undef,
+  $ns_worm = undef,
+  $meta2_max_versions = undef,
   $ns_service_update_policy     = {
     'meta2' => 'KEEP|3|1|',
     'sqlx'  => 'KEEP|1|1|',
@@ -50,6 +81,37 @@ define openiosds::namespace (
   if $ns_storage_policy { validate_string($ns_storage_policy) }
   if $ns_chunk_size { validate_integer($ns_chunk_size) }
   if $ns_service_update_policy { validate_hash($ns_service_update_policy) }
+  if $sqliterepo_election_delay_ping_final { validate_string($sqliterepo_election_delay_ping_final) }
+  if $sqliterepo_election_delay_expire_slave { validate_string($sqliterepo_election_delay_expire_slave) }
+  if $sqliterepo_election_delay_expire_master { validate_string($sqliterepo_election_delay_expire_master) }
+  if $sqliterepo_election_delay_expire_none { validate_string($sqliterepo_election_delay_expire_none) }
+  if $sqliterepo_outgoing_timeout_cnx_use { validate_string($sqliterepo_outgoing_timeout_cnx_use) }
+  if $sqliterepo_outgoing_timeout_req_use { validate_string($sqliterepo_outgoing_timeout_req_use) }
+  if $sqliterepo_outgoing_timeout_cnx_getvers { validate_string($sqliterepo_outgoing_timeout_cnx_getvers) }
+  if $sqliterepo_outgoing_timeout_req_getvers { validate_string($sqliterepo_outgoing_timeout_req_getvers) }
+  if $sqliterepo_outgoing_timeout_cnx_replicate { validate_string($sqliterepo_outgoing_timeout_cnx_replicate) }
+  if $sqliterepo_outgoing_timeout_req_replicate { validate_string($sqliterepo_outgoing_timeout_req_replicate) }
+  if $sqliterepo_outgoing_timeout_cnx_resync { validate_string($sqliterepo_outgoing_timeout_cnx_resync) }
+  if $sqliterepo_outgoing_timeout_req_resync { validate_string($sqliterepo_outgoing_timeout_req_resync) }
+  if $sqliterepo_election_wait_delay { validate_string($sqliterepo_election_wait_delay) }
+  if $sqliterepo_election_wait_quantum { validate_string($sqliterepo_election_wait_quantum) }
+  if $sqliterepo_election_nowait_after { validate_string($sqliterepo_election_nowait_after) }
+  if $proxy_outgoing_timeout_stat { validate_string($proxy_outgoing_timeout_stat) }
+  if $proxy_outgoing_timeout_conscience { validate_string($proxy_outgoing_timeout_conscience) }
+  if $proxy_outgoing_timeout_common { validate_string($proxy_outgoing_timeout_common) }
+  if $meta0_outgoing_timeout_common_req { validate_string($meta0_outgoing_timeout_common_req) }
+  if $meta1_outgoing_timeout_common_req { validate_string($meta1_outgoing_timeout_common_req) }
+  if $gridd_timeout_connect_common { validate_string($gridd_timeout_connect_common) }
+  if $resolver_cache_srv_max_default { validate_string($resolver_cache_srv_max_default) }
+  if $resolver_cache_csm0_max_default { validate_string($resolver_cache_csm0_max_default) }
+  if $sqliterepo_cache_ttl_cool { validate_string($sqliterepo_cache_ttl_cool) }
+  if $sqliterepo_cache_ttl_hot { validate_string($sqliterepo_cache_ttl_hot) }
+  if $client_errors_cache_enabled { validate_string($client_errors_cache_enabled) }
+  if $client_errors_cache_period { validate_string($client_errors_cache_period) }
+  if $client_errors_cache_max { validate_string($client_errors_cache_max) }
+  if $client_down_cache_avoid { validate_bool($client_down_cache_avoid) }
+  if $ns_worm { validate_bool($ns_worm) }
+  if $meta2_max_versions { validate_string($meta2_max_versions) }
 
   if $openiosds::action == 'create' {
     # Path

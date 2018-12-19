open-io/puppet-openiosds

commit d9a789bc78f1ee6148c47357e568ee67117250b0

Update conscience.pp

manifests/conscience.pp
@@ -16,18 +16,21 @@ define openiosds::conscience (
     'SINGLE'       => 'NONE:NONE',
     'TWOCOPIES'    => 'NONE:DUPONETWO',
     'THREECOPIES'  => 'NONE:DUPONETHREE',
+    'DUPONEFOUR'   => 'NONE:DUPONEFOUR',
     'ERASURECODE'  => 'NONE:ERASURECODE',
     'ECLIBEC42D1'  => 'NONE:ECLIBEC42D1',
     'ECLIBEC63D1'  => 'NONE:ECLIBEC63D1',
     'ECLIBEC144D1' => 'NONE:ECLIBEC144D1',
     'ECISAL42D1'   => 'NONE:ECISAL42D1',
     'ECISAL63D1'   => 'NONE:ECISAL63D1',
+    'ECISAL84D1'   => 'NONE:ECISAL84D1',
     'ECISAL144D1'  => 'NONE:ECISAL144D1',
     'ECISALC35D1'  => 'NONE:ECISALC35D1',
     'ECISALC75D1'  => 'NONE:ECISALC75D1'},
   $data_security                         = {
     'DUPONETWO'    => 'plain/distance=1,nb_copy=2',
     'DUPONETHREE'  => 'plain/distance=1,nb_copy=3',
+    'DUPONEFOUR'   => 'plain/distance=1,nb_copy=4',
     'ERASURECODE'  => 'ec/k=6,m=3,algo=liberasurecode_rs_vand,distance=1',
     'ECLIBEC63D1'  => 'ec/k=6,m=3,algo=liberasurecode_rs_vand,distance=1',
     'ECLIBEC42D1'  => 'ec/k=4,m=2,algo=liberasurecode_rs_vand,distance=1',
@@ -35,6 +38,7 @@ define openiosds::conscience (
     'ECLIBEC144D1' => 'ec/k=14,m=4,algo=liberasurecode_rs_vand,distance=1',
     'ECISAL42D1'   => 'ec/k=4,m=2,algo=isa_l_rs_vand,distance=1',
     'ECISAL63D1'   => 'ec/k=6,m=3,algo=isa_l_rs_vand,distance=1',
+    'ECISAL84D1'   => 'ec/k=8,m=4,algo=isa_l_rs_vand,distance=1,
     'ECISAL123D1'  => 'ec/k=12,m=3,algo=isa_l_rs_vand,distance=1',
     'ECISAL144D1'  => 'ec/k=14,m=4,algo=isa_l_rs_vand,distance=1',
     'ECISALC35D1'  => 'ec/k=3,m=5,algo=isa_l_rs_cauchy,distance=1',

commit dd37311c1bfa7ab50f9241fd4042313e80a6bed5

Add missing quote leading to a puppet manifest parsing error
Error: Syntax error at 'ECISAL123D1'; expected '}' at /usr/share/puppet/modules/openiosds/manifests/conscience.pp:42

Fixes: d9a789bc78f1ee6148c47357e568ee67117250b0

manifests/conscience.pp
@@ -38,7 +38,7 @@ define openiosds::conscience (
     'ECLIBEC144D1' => 'ec/k=14,m=4,algo=liberasurecode_rs_vand,distance=1',
     'ECISAL42D1'   => 'ec/k=4,m=2,algo=isa_l_rs_vand,distance=1',
     'ECISAL63D1'   => 'ec/k=6,m=3,algo=isa_l_rs_vand,distance=1',
-    'ECISAL84D1'   => 'ec/k=8,m=4,algo=isa_l_rs_vand,distance=1,
+    'ECISAL84D1'   => 'ec/k=8,m=4,algo=isa_l_rs_vand,distance=1',
     'ECISAL123D1'  => 'ec/k=12,m=3,algo=isa_l_rs_vand,distance=1',
     'ECISAL144D1'  => 'ec/k=14,m=4,algo=isa_l_rs_vand,distance=1',
     'ECISALC35D1'  => 'ec/k=3,m=5,algo=isa_l_rs_cauchy,distance=1',